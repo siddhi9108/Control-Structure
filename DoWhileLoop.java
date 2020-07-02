335adf6354bcc9e6b130c5b8751f93510da5ad3b
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-02 21:05:34 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,28 @@
+package javaapplication2;
+
+import java.util.Scanner;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 12;
+        System.out.println("Do while");
+        do {
+            System.out.println("Hello");
+
+            i++;
+
+        } while (i < 5);
+        i = 12;
+        System.out.println("While loop");
+        while (i < 5) {
+            System.out.println("Hello");
+
+            i++;
+
+        }
+
+    }
+
+}
